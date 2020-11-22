da35dbb18c804be43a040f6b81ef2da438ee74e6
sunao <2534658059@qq.com>
2020-11-22 23:19:12 +0800

    user setting

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,47 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 
 def home(request):
-    return HttpResponse('hello world')
+    boards = Board.objects.all()
+    return render(request, 'boards/index.html', {'boards': boards})
+
+
+def about(request):
+    # do something
+    return render(request, 'about.html')
+
+
+def about_company(request):
+    # do something
+    return render(request, 'about_company.html', {'company_name': 'Simple Complex'})
+
+
+def topics(request, pk):
+    # board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics/index.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            user = User.objects.first()
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'topics/new.html', {'form': form, 'board': board})
--- myproject/settings.py
+++ myproject/settings.py
@@ -35,8 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'widget_tweaks',
+
     # app
     'boards',
+    'accounts',
 
 ]
 
@@ -55,7 +59,9 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'template')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
+
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
